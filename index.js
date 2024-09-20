6f2f86112a63570641859432f4a1497c3095f737
Arafat islam <arafat.innotimes.net@gmail.com>
2024-09-20 15:19:33 +0600

    Booking Route Amplement

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 
-require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/dbConfig');
+require('dotenv').config();
+const bookingsRoute = require('./Modules/booking/bookingsRoute');
+// middleware
 const app = express();
 
 connectDB();
 
 //Middleware
 app.use(express.json());
-  
+
+//Routes
+
+app.use('/api/bookings', bookingsRoute);
+
+
+
 //Home
 app.get('/', (req, res) => {
   res.send("Server is okay")
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
