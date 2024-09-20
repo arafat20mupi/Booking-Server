b3797b69271026cf5a32715e711587100a3b5358
Arafat islam <arafat.innotimes.net@gmail.com>
2024-09-20 14:55:44 +0600

    Error fixed

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
-const connectDB = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology: true
-        });
-        console.log("mongodb connected");
-        
-    } catch (error) {
-        console.log(error.message)
-        process.exit(1);
-    }
-}
 
-module.exports =connectDB;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI); // No need for useNewUrlParser or useUnifiedTopology
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit with failure
+  }
+};
+
+module.exports = connectDB;
